b91748f1bfaf1d71cdab96c0f4bc427f7b05bd12
moggymog <mohamede233@gmail.com>
2022-05-15 16:11:23 -0700

    1 file modified

--- pages/index.js
+++ pages/index.js
@@ -748,7 +748,7 @@ export default function Example() {
                   <div className="sm:text-center lg:text-left">
                     <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
                       <span className="block xl:inline">
-                        Welcome to the Used Grade
+                        Welcome to the UsedGrade
                       </span>{' '}
                       <span className="block text-indigo-600 xl:inline">
                         Marketplace
