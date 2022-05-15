138c1a8d43b7139786b0ba78c7900e622e3212f4
moggymog <mohamede233@gmail.com>
2022-05-15 16:15:27 -0700

    1 file modified

--- pages/index.js
+++ pages/index.js
@@ -782,7 +782,7 @@ export default function Example() {
             <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
               <img
                 className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
-                src="https://gitlab.com/moggymog/4way/-/raw/master/public/assets/electronic_warehouse.png"
+                src="https://gitlab.com/moggymog/4way/-/raw/master/public/assets/warehouse.jpeg"
                 alt=""
               />
             </div>
