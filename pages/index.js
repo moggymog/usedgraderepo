fd8fc3cf0f575864f21bd5f5d22efda914f59e9b
moggymog <mohamede233@gmail.com>
2022-05-16 16:30:14 -0700

    1 file modified

--- pages/index.js
+++ pages/index.js
@@ -514,10 +514,12 @@ export default function Example() {
 
   return (
     <>
-      <meta
-        name="facebook-domain-verification"
-        content="eveuxypjl1h26al88rva9b2nc0zh8p"
-      />
+      <head>
+        <meta
+          name="facebook-domain-verification"
+          content="eveuxypjl1h26al88rva9b2nc0zh8p"
+        />
+      </head>
       <Script
         strategy="lazyOnload"
         src={`https://www.googletagmanager.com/gtag/js?id=G-X2HB7B4JHT`}
